d38607dd4d99c2b8c77d2f4d1c82e6539c12946f
ShunyiZhao <shunyizhao@mail.nankai.edu.cn>
2020-05-25 00:58:41 +0800

    prepared style

--- Server/routes/getWorks.js
+++ Server/routes/getWorks.js
@@ -6,71 +6,92 @@ var sql = require('sqlite3');
 var db = new sql.Database("serverData.db");
 var result;
 
-var baseQuery = "select * from work";
-var WHERE = " where";
-var whereFlag = false;
+var baseQuery = "select * from work where ( type = ? OR type = ? ) AND (subject = ? OR subject = ? OR subject = ? OR subject = ?)";
 
 router.get('/', function (req, res, next) {
+    var ps = db.prepare(baseQuery);
+    //construct query
     var typeFlag = false;
     var subjectFlag = false;
-    whereFlag = false;
-    //construct query
-    var newQuery = baseQuery;
+    var typeStr = "";
+    var subjectStr = "";
+    //select the patent or paper
     if(req.query.flags[0] == 1){
-        newQuery = checkFlag(newQuery);
-        newQuery += " type = 'patent'";
         typeFlag = true;
-    };
-    if(req.query.flags[1] == 1 && !typeFlag){
-        newQuery = checkFlag(newQuery);
-        newQuery += " type = 'paper'";
-    };
+        typeStr += "patent";
+    }
+    if(!typeFlag && req.query.flags[1] == 1){
+        typeFlag = true;
+        typeStr += "paper";
+    }
+    //select the subject
     if(req.query.flags[2] == 1){
-        newQuery = checkFlag(newQuery);
-        newQuery += " subject = 'artificial'";
         subjectFlag = true;
-    };
-    if(req.query.flags[3] == 1 && !subjectFlag){
-        newQuery = checkFlag(newQuery);
-        newQuery += " subject = 'data'";
+        subjectStr = "artificial";
+    }
+    if(!subjectFlag && req.query.flags[3] == 1){
         subjectFlag = true;
-    };
-    if(req.query.flags[4] == 1 && !subjectFlag){
-        newQuery = checkFlag(newQuery);
-        newQuery += " subject = 'math'";
+        subjectStr = "data";
+    }
+    if(!subjectFlag && req.query.flags[4] == 1){
         subjectFlag = true;
-    };
-    if(req.query.flags[5] == 1 && !subjectFlag){
-        newQuery = checkFlag(newQuery);
-        newQuery += " subject = 'robo'"
-    };
-    console.log(newQuery);
+        subjectStr = "math";
+    }
+    if(!subjectFlag && req.query.flags[5] == 1){
+        subjectFlag = true;
+        subjectStr = "robo";
+    }
+    console.log(typeStr);
+    console.log(subjectStr);
     
-    getWork(newQuery, function () {
+    getWork(ps, typeStr, subjectStr, function () {
         console.log(result);
-        res.send("success!");
+        res.send(result);
     })
 })
 
-var getWork = function (query, callback){
-    db.all(query, function (error, rows) {
-        if (error) throw error;
-        if (rows) {
-            result = rows;
-        }
-        callback();
-    });
-};
-
-var checkFlag = function(query) {
-    if(whereFlag === false){
-        query += WHERE;
-        whereFlag = true;
+var getWork = function (preQuery, strType, strSubject, callback) {
+    //no select
+    //console.log("enter a");
+    if(strType === "" && strSubject === ""){
+        preQuery.all("patent", "paper", "artificial", "data", "math", "robo", function (error, rows) {
+            if (error) throw error;
+            if (rows) {
+                result = rows;
+            }
+            callback();
+        });
     }
-    else {
-        query += " AND";
+    //select type & no select sub
+    if(strType != "" && strSubject ==="") {
+        preQuery.all(strType, strType, "artificial", "data", "math", "robo", function (error, rows) {
+            if (error) throw error;
+            if (rows) {
+                result = rows;
+            }
+            callback();
+        });
     }
-    return query;
-};
+    // no select type * select sub
+    if(strType === "" && strSubject != "") {
+        preQuery.all("patent", "paper", strSubject, strSubject, strSubject, strSubject, function (error, rows) {
+            if (error) throw error;
+            if (rows) {
+                result = rows;
+            }
+            callback();
+        });
+    }
+    // select type & sub
+    if (strType != "" && strSubject != "" ) {
+        preQuery.all(strType, strType, strSubject, strSubject, strSubject, strSubject, function (error, rows) {
+            if (error) throw error;
+            if (rows) {
+                result = rows;
+            }
+            callback();
+        });
+    }
+}
 
 module.exports = router;
